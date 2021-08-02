51a3ca9782034763b9a2d85c55fb1d390f12549a
Abdulkarim Saddik <abdulkarim.saddik@gmail.com>
2021-08-02 17:58:32 +0200

    update the script and README.md

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # Bash Script for String Math Operations and Concatenation
 
-This script concatenates, adds, substracts or multiplies the passed parameters and prints the result to the stdout. The parameters can be passed from a pipe to it. The operation is specified depending on the control option passed to the script.
+This script concatenates, adds, substracts, divides or multiplies the passed parameters and prints the result to the stdout. The parameters can be passed from a pipe to it. The operation is specified depending on the control option passed to the script. Float numbers are supported.
 
 ## Syntax
 
 ```
-user@mypc:~/Bash-Script-for-Dynamically-Math-Calculations$ ./cal.sh <option> [parameter1 parameter2 ..] 
+./cal.sh <option> [parameter1 parameter2 ..] 
 ```
 
 ## Wokflow
 
-- Clone the script to your PC. [URL Link](git@github.com:Abdsaddik/Bash-Script-for-Dynamically-Math-Calculations.git)
+- Clone the script to your PC.
+
+```
+git clone https://github.com/Abdsaddik/Bash-Script-for-Dynamically-Math-Calculations.git
+```
 - Go inside the the cloned folder:
 
 ```
@@ -48,6 +52,28 @@ user@mypc:~/Bash-Script-for-Dynamically-Math-Calculations$ ./calc.sh -sum -10 10
 10
 ```
 
+### Multiplication
+
+Multiplication is activated by the option **-mul** as follows:
+
+```
+user@mypc:~/Bash-Script-for-Dynamically-Math-Calculations$ ./calc.sh -mul 2.5 -2 -3
+15.0
+```
+
+This multiplies the number 2.5 by -2 and -3 anr returns the result.
+
+### Division
+
+Similar to multiplication, the first parameter is devided on all followed passed parameters. The option needed is **-div**. Follows is an example:
+
+```
+user@mypc:~/Bash-Script-for-Dynamically-Math-Calculations$ ./calc.sh -div 80 10 2 2
+2.000
+user@mypc:~/Bash-Script-for-Dynamically-Math-Calculations$ seq 3 | ./calc.sh -div
+.166
+```
+
 ### String Concatenation
 
 To cacatenate strings, the option **-con** is passed to the script
@@ -70,13 +96,14 @@ user@mypc:~/Bash-Script-for-Dynamically-Math-Calculations$ seq 6 | ./calc.sh -mu
 
 ### Available options
 
-Type the following line to show which options are available
+Type the following command to show which options are available
 
 ```
 user@mypc:~/Bash-Script-for-Dynamically-Math-Calculations$ ./calc -h
 supported parameters are:
 -add            calculate the value of adding the passed parameters
 -mul            calculate the value of multiplying the passed parameters
+-div            divide the first parameter to the rest of parameters
 -con            concatenate the passed parameters together
 -h or --help    print help command
 ```
--- calc.sh
+++ calc.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# if the number of passed parameters is equal to zero
-# -> no parameters are passed -> exit the script with error value 1
-empty(){
-if [ "$#" -lt 2 ]
-then
-        echo "too few parameters"
-        return 1
-fi
-}
+# check the syntax of the passed parameters
 check_syntax(){
 if ! [[ "$1" =~ ^[+-]?[0-9]+?[.]?[0-9]+$ ]]
         then
@@ -22,13 +14,26 @@ if [[ "$var" =~ ^[+][0-9]+$ ]]
 fi
 }
 
+# if the number of passed parameters is equal to zero
+# -> no parameters are passed -> exit the script with error value 1
+# check the syntax correctness of the passed parameters 
+check(){
+if [ "$#" -lt 2 ]
+then
+        return 1
+fi
+for var; do check_syntax $var; if test $? -ne 0; then 
+        return 1
+        fi 
+done
+}
+
 # loop over the passed parameters and check if they are integers
 # in case of false -> bash scipt stops and returns error value 1
 # in cae of true -> SUM value is added to the next parameter
 sum(){
 VAL=0
 for var;do
-        check_syntax $var
         VAL=$( bc <<< $VAL+$var )
 done
 echo $VAL
@@ -38,22 +43,29 @@ echo $VAL
 multi(){
 VAL=1
 for var;do
-        check_syntax $var
-        # in case the user passed the sign + , then remove it to avoid errors by the command bc
-        # if [[ "$var" =~ ^[+][0-9]+$ ]]; then var=${var#+}; fi
         VAL=$( bc -l <<< "$VAL*$var" )
 done
 echo $VAL
 }
 
+# devide the first passed parameter on the followed passed parameters 
+# one after one und print the result to the stdout
+div(){
+VAL=$1
+while [ "$#" -gt 1 ]; do shift;
+        VAL=$(bc -l <<< "scale=3; $VAL/$1")
+done
+echo $VAL 
+}
+
 # concat the passed parameters together
 concat(){
-        val="$1"
-        while test "$#" -gt 0; do
-                shift 1
-                val+="$1"
-        done
-        echo "$val"
+val="$1"
+while test "$#" -gt 0; do
+        shift 1
+        val+="$1"
+done
+echo "$val"
 }
 #################################################################
 
@@ -68,7 +80,6 @@ if [ -p /dev/stdin ]; then
         done
 
 else
-
 # save the passed parameters except the first parameter $1 to the array ARR
         while [ "$#" -gt 0 ]; do
                 if [ "$cnt" -gt 0 ]; then
@@ -79,34 +90,37 @@ else
         done
 fi
 
+if test "$val" = "-con"; then concat ${ARR[*]};  
 
-case "$val" in
--add | -sum)
-        empty ${ARR[*]}
-        sum ${ARR[*]}
-        exit 0
-        ;;
--mul)
-        empty ${ARR[*]}
-        multi ${ARR[*]}
-        exit 0
-        ;;
--con)
-        concat ${ARR[*]}
-        exit 0
-        ;;
--h | --help)
+elif [ "$val" = "-h" ] || [ "$val" = "--help" ] && [ ${#ARR[@]} -eq 0 ]; then
         echo "supported parameters are:
 -add            calculate the value of adding the passed parameters
 -mul            calculate the value of multiplying the passed parameters
+-div            divide the first parameter to the rest of parameters
 -con            concatenate the passed parameters together
 -h or --help    print help command"
 
-        exit 0
+
+elif ! check ${ARR[*]} ; then         
+        echo "too few parameters"
+        exit 1
+else
+case "$val" in
+-add | -sum)
+        sum ${ARR[*]}
+        ;;
+-mul)
+        multi ${ARR[*]}
+        ;;
+-div)   
+        div ${ARR[*]}
         ;;
 *)
-        echo "invalid parameter is passed"; exit 1
+        echo "invalid parameter is passed"
+        exit 1
         ;;
-"")     echo "you should pass an option!"; exit 1
+"")     echo "you should pass an option!" 
+        exit 1
         ;;
 esac
+fi
